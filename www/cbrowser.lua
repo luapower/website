2229c109f02ee8f7f756ca962ec17b110ae31301
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-06-18 17:06:28 +0300

    unimportant

--- www/cbrowser.lua
+++ www/cbrowser.lua
@@ -1,7 +1,8 @@
 
-local cb = {}
+local glue = require'glue'
+--local cpp = require'cpp'
 
-local src_dir = '/Users/cosmin/luajit-cscope/luajit-2.1/src'
+local cb = {}
 
 local function idfy(s)
 	return s:gsub('[./]', '_')
@@ -11,96 +12,149 @@ local cats = {
 	{'Util',          'lj_buf.c', 'lj_alloc.c', 'lj_char.c', 'lj_def.h', },
 	{'Parsing',       'lj_lex.c', 'lj_parse.c', },
 	{'Bytecode',      'lj_bc.c', 'lj_bcdump.h', },
-	{'IR Emitter',    'lj_ir.c', 'lj_ircall.h', 'lj_iropt.h', }, 
+	{'IR Emitter',    'lj_ir.c', 'lj_ircall.h', 'lj_iropt.h', },
 	{'IR -> ASM',     'lj_asm.c', 'lj_asm_arm.h', 'lj_asm_mips.h', 'lj_asm_ppc.h', 'lj_asm_x86.h'},
 	{'Interpreter',   'lj_dispatch.c', },
-	{'Compiler',      
-		'lj_jit.h', 'lj_mcode.c', 'lj_snap.c', 
-		'lj_record.c', 'lj_crecord.c', 'lj_ff.h', 'lj_ffrecord.c', 'lj_trace.c', 'lj_traceerr.h', 
+	{'Compiler',
+		'lj_jit.h', 'lj_mcode.c', 'lj_snap.c',
+		'lj_record.c', 'lj_crecord.c', 'lj_ff.h', 'lj_ffrecord.c', 'lj_trace.c', 'lj_traceerr.h',
 	},
-	{'Assembler',     
-		'lj_arch.h', 
+	{'Assembler',
+		'lj_arch.h',
 		'lj_target.h', 'lj_target_arm.h', 'lj_target_arm64.h', 'lj_target_mips.h', 'lj_target_ppc.h', 'lj_target_x86.h',
-		'lj_emit_arm.h', 'lj_emit_mips.h', 'lj_emit_ppc.h', 'lj_emit_x86.h', },  
+		'lj_emit_arm.h', 'lj_emit_mips.h', 'lj_emit_ppc.h', 'lj_emit_x86.h', },
 	{'Errors',        'lj_err.c', 'lj_errmsg.h', },
 	{'GC',            'lj_gc.c', },
 	{'Debugging',     'lj_debug.c', 'lj_gdbjit.c', },
-	{'Lua API',       'lj_lib.c', 'lj_tab.c', 'lj_str.c', 'lj_strscan.c', 'lj_strfmt.c', }, 
+	{'Lua API',       'lj_lib.c', 'lj_tab.c', 'lj_str.c', 'lj_strscan.c', 'lj_strfmt.c', },
 	{'Lua C API',     'lualib.h', 'lauxlib.h', 'lua.h', 'luajit.h', 'lua.hpp', },
 	{'C Data',        'lj_carith.c', 'lj_cconv.c', 'lj_cdata.c', 'lj_cparse.c', 'lj_ctype.c', },
 	{'FFI',           'lj_ccall.c', 'lj_ccallback.c', 'lj_clib.c'},
 	{'Profiler',      'lj_profile.c', },
-	{'Frontend',      'luajit.c', }, 
-	{'Building',      'luaconf.h', 'ps4build.bat', }, 
+	{'Frontend',      'luajit.c', },
+	{'Building',      'luaconf.h', 'ps4build.bat', },
 }
 
 local function parse_cscope_db(filename)
+	local yield = coroutine.yield
+	return coroutine.wrap(function()
+		local lineno = 0
+		local getline = io.lines(filename)
+		local get = function()
+			lineno = lineno + 1
+			return getline()
+		end
+		assert(get()) --header
+		local fileline = get()
+		while fileline do
+			local file = fileline:match'^\t@(.*)'
+			if file == '' then break end --database footer
+			assert(file, lineno..': '..fileline)
+			yield('@', file)
+			assert(get()) --mandatory empty line
+			local lineline = get()
+			while lineline do --lines
+				local line, text = lineline:match'^(%d+) (.*)'
+				if not line then --no more lines: next file?
+					fileline = lineline
+					break
+				end
+				yield(':', tonumber(line))
+				local ifend
+				if text == '#if ' then
+					yield('if')
+				elseif text == '#ifdef ' then
+					yield('if')
+					yield('', 'defined')
+					yield('', '(')
+					ifend = ')'
+				elseif text == '#ifndef ' then
+					yield('if')
+					yield('', '!')
+					yield('', 'defined')
+					yield('', '(')
+					ifend = ')'
+				elseif text == '#elif' then
+					yield('elif')
+				elseif text == '#else' then
+					yield('else')
+				else
+					yield('', text)
+				end
+				local symline = get()
+				while symline do --symbols
+					if symline == '' then --no more symbols: next line?
+						lineline = get()
+						if lineline:find'^\t%)' then --exception: end def
+							yield('enddef')
+							assert(get() == '') --mandatory empty line
+							lineline = get()
+						end
+						break
+					end
+					local mark, text = symline:match'^\t(.)(.*)'
+					local nonsymline = get() --non-symbol
+					assert(nonsymline)
+					if mark == '#' then
+						if nonsymline == '(' then
+							yield('macro', text)
+						else
+							yield('define', text)
+						end
+					elseif mark == '~' then
+						local s, file = text:match'(.)(.*)'
+						yield('include', file, s)
+					elseif text then
+						yield(mark, text)
+					else
+						yield('S', symline)
+					end
+					coroutine.yield('', nonsymline)
+					symline = get()
+				end
+				if ifend then
+					yield('', ifend)
+					yield('ifend')
+				end
+			end
+		end
+	end)
+end
+
+local function if_tree(cscope_iter)
 	local t = {}
-	local state
-	local ft, line
-	local function tagline(fname)
-		local f = io.open(src_dir..'/'..fname)
-		f:read'*l'
-		local s = f:read'*l'
-		f:close()
-		return s:match'^%s*%*%*%s*(.-)%.?$'
-	end
-	local function addfile(f) 
-		if f == '' then return end 
-		ft = {name = f, id = idfy(f), defs = {}, tagline = tagline(f)}
-		table.insert(t, ft)
-	end
-	local function adddef(text)
-		table.insert(ft.defs, {text = text, line = line})
-	end
-	local function addtext(text, mark, bol)
-		if mark and mark:find'[%#%$cegtsul]' then
-			adddef(mark..' '..text)
-		end
-		if bol and text:find'^#' then
-			local s = text:match'^#(.*)'
-			if s == 'if' then
-
-			end
+	local file, line, branch, expr
+	local function close_branches() --the cscope db doesn't keep '#endif's!
+		while branch.parent do
+			branch.line2 = line
+			branch = branch.parent
 		end
 	end
-	local i = 0
-	local function checkmark(s)
-		local mark, text = s:match'^\t(.)(.*)'
-		if not mark then return end
-		indefine = mark == '#'
-		infunc = mark == '`'
-		if mark == '@' then
-			addfile(text)
+	for what, text in cscope_iter do
+		if what == '@' then
+			close_branches()
+			branch = {line1 = line, branches = {}}
+			file = {name = text, branch = branch}
+			table.insert(t, file)
+		elseif what == ':' then
+			line = text
+		elseif what == 'if' then
+			expr = {}
+		elseif what == 'ifend' or what == 'else' then
+			local newbranch = {line1 = line, expr = expr, branches = {}, parent = branch}
+			table.insert(branch.branches, newbranch)
+			branch = newbranch
+			expr = nil
+		elseif expr then
+			table.insert(expr, text)
 		else
-			addtext(text, mark)
-		end
-		return true
-	end
-	local function checkline(s)
-		local text
-		line, text = s:match'^(%d+) (.*)'
-		assert(line, s)
-		addtext(text, nil, true)
-	end
-	for s in io.lines(filename) do
-		i = i + 1
-		--if i > 1000 then break end 
-		if not state then
-			state = 'check'
-		elseif s == '' then
-			state = 'check'
-		elseif state == 'check' then
-			local _ = checkmark(s) or checkline(s)
-			state = 'sym'
-		elseif state == 'sym' then
-			if not checkmark(s) then
-				local text = s
-				addtext(text)
-			end
 		end
 	end
+	close_branches()
+	return t
+end
 
+local function file_list()
 	local map = {}
 	for _,f in ipairs(t) do
 		map[f.name] = f
@@ -138,12 +192,12 @@ local function parse_cscope_db(filename)
 		local ct = {catname = catname, files = {}, n = 0}
 		table.insert(ddt, ct)
 		for i=2,#cat do
-			local f = map[cat[i]] 
+			local f = map[cat[i]]
 			if f then
 				table.insert(ct.files, f)
 				f.cat = catname
-				if f.list then 
-					ct.n = ct.n + 1 
+				if f.list then
+					ct.n = ct.n + 1
 					if ct.n == 1 then
 						f.first = true
 					end
@@ -156,7 +210,7 @@ local function parse_cscope_db(filename)
 	for _,f in ipairs(dt) do
 		if not f.cat then
 			table.insert(ct.files, f)
-			if f.list then 
+			if f.list then
 				ct.n = ct.n + 1
 				if ct.n == 1 then
 					f.first = true
@@ -164,7 +218,7 @@ local function parse_cscope_db(filename)
 			end
 		end
 	end
-	
+
 	return ddt
 end
 
@@ -174,9 +228,39 @@ end
 
 local db
 
-return function(what, ...)
+local function cbrowser(what, ...)
 	what = what or 'index'
 	local handler = cb[what] or cb.index
-	db = db or parse_cscope_db'/Users/cosmin/luajit-cscope/luajit-2.1.cscope'
+	db = db or parse_cscope_db()
 	return handler(db, ...)
+end
+
+
+if not ... then
+	local src_dir = '/Users/cosmin/luajit-cscope/luajit-2.1/src'
+	local cscope_db = '/Users/cosmin/luajit-cscope/luajit-2.1.cscope'
+	local pp = require'pp'
+
+	local function test_parsing()
+		local cmd = parse_cscope_db(cscope_db)
+		local function pass(...)
+			if ... then
+				print(...)
+				return true
+			else
+				return false
+			end
+		end
+		for i = 1, 1000000 do
+			if not pass(cmd()) then break end
+		end
+	end
+
+	local function test_if_tree()
+		pp(if_tree(parse_cscope_db(cscope_db)))
+	end
+
+	test_parsing()
+	--test_if_tree()
+
 end
